669e8f395196da0d1158bbb4cc9388d65870ed73
Abhay <abhay.ciem@gmail.com>
2018-04-04 00:28:19 +0530

    test
    test

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+//var port_number = (process.env.PORT || 3000);
 
-var port_number = process.env.PORT || 3000;
-
-app.listen(port_number, () => console.log('Example app listening on port 3000!'))
+app.listen((process.env.PORT || 3000), () => console.log('Example app listening on port 3000!'))
